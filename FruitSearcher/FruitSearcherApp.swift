e1010bd3e1c3b0312614d3c3c80c9860d9bc04a9
Carolane <107761189+CarolaneLFBV@users.noreply.github.com>
2022-09-30 10:36:03 +0200

    Initial Commit

---
+++ FruitSearcher/FruitSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitSearcherApp.swift
+//  FruitSearcher
+//
+//  Created by Carolane on 30/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
