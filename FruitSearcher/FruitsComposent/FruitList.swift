a1758d8ab140896ce3581eb69b699aa7b8161d94
Carolane <107761189+CarolaneLFBV@users.noreply.github.com>
2022-09-30 13:55:07 +0200

    Searchbar working

--- FruitSearcher/FruitsComposent/FruitList.swift
+++ FruitSearcher/FruitsComposent/FruitList.swift
@@ -11,29 +11,38 @@ struct FruitList: View {
     @State var fruits = [Fruit]()
     @State private var searchText = ""
     
+    var searchResults: [Fruit] {
+        if searchText.isEmpty {
+            return fruits
+        } else {
+            return fruits.filter { $0.name == searchText}
+        }
+    }
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Recherche : \(searchText)")
-                    .font(.system(size: 13, weight: .bold))
-                    .searchable(text: $searchText, prompt: "Veuillez entrer un fruit")
-                    .navigationTitle("Liste des fruits")
-                    
-                List(fruits) { fruit in
-                    NavigationLink(destination: FruitInfos(fruit: fruit)) {
-                        HStack {
-                            Image(fruit.name)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50 , height: 50)
-                                .padding()
-                            
-                            Text(fruit.name)
-                                .font(.system(size: 20, weight: .medium))
+                List {
+                    ForEach(searchResults) { fruit in
+                        NavigationLink(destination: FruitInfos(fruit: fruit)) {
+                            HStack {
+                                Image(fruit.name)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50 , height: 50)
+                                    .padding()
+                                
+                                Text(fruit.name)
+                                    .font(.system(size: 20, weight: .medium))
+                            }
                         }
                     }
                 }
-            }.onAppear {
+                .searchable(text: $searchText)
+                .navigationTitle("Liste des fruits")
+                
+            }
+            .onAppear {
                 FruitAPI().loadData { (fruits) in
                     self.fruits = fruits
                 }
