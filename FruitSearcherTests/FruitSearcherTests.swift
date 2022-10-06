148d206174f6d8fea41e27c971adc731f3c395dd
Carolane <107761189+CarolaneLFBV@users.noreply.github.com>
2022-10-06 09:23:56 +0200

    Added Tests

--- FruitSearcherTests/FruitSearcherTests.swift
+++ FruitSearcherTests/FruitSearcherTests.swift
@@ -10,6 +10,8 @@ import XCTest
 
 final class FruitSearcherTests: XCTestCase {
 
+    var fruit: Fruit?
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,12 +20,20 @@ final class FruitSearcherTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testId() {
+        XCTAssertEqual(fruit?.id, 6)
+    }
+    
+    func testName() {
+        XCTAssertEqual(fruit?.name, "Apple")
+    }
+    
+    func testFamily() {
+        XCTAssertEqual(fruit?.family, "Rosaceae")
+    }
+    
+    func testOrder() {
+        XCTAssertEqual(fruit?.order, "Rosales")
     }
 
     func testPerformanceExample() throws {
